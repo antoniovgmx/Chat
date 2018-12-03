2db196a3eef2fee39a44f96397f1f42f60f790ed
Aixa <aixa.lievana@gmail.com>
2018-12-03 15:34:00 -0600

    nuevo
    css

--- public/js/funciones.js
+++ public/js/funciones.js
@@ -5,27 +5,29 @@
  window.addEventListener("resize",function () {
     wid = document.body.clientWidth;
       console.log(wid);
-
+        document.getElementById('abajo').style.display = "block";
       var co = document.getElementById("CONVER")
-      if (co.style.display = "block" && wid < 768) {
+      if ((co.style.display ="block") && (wid < 768)) {
           document.getElementById('abajo').style.display = "none";
+      }else{
+          document.getElementById('abajo').style.display = "block";
       }
  });
 
 function chats(){
     if(wid<768){
-        document.getElementById("chat").style.color = "brown"
-        document.getElementById("perfil").style.color = "gray";
-        document.getElementById("conver").style.color = "gray";
+        document.getElementById("chat").style.color = "coral"
+        document.getElementById("perfil").style.color = "white";
+        document.getElementById("conver").style.color = "white";
 
 
         document.getElementById("CHATS").style.display = "block";
         document.getElementById("CONTACTOS").style.display = "none";
         document.getElementById("PERFIL").style.display = "none";
     }else{
-       document.getElementById("chat").style.color = "brown"
-       document.getElementById("perfil").style.color = "gray";
-       document.getElementById("conver").style.color = "gray";
+       document.getElementById("chat").style.color = "coral"
+       document.getElementById("perfil").style.color = "gainsboro";
+       document.getElementById("conver").style.color = "gainsboro";
 
        document.getElementById("CONVER").style.display="block"
        document.getElementById("CHATS").style.display = "block";
@@ -37,18 +39,18 @@ function chats(){
 
 function contac(){
     if(wid<768){
-        document.getElementById("conver").style.color = "brown"
-        document.getElementById("perfil").style.color = "gray";
-        document.getElementById("chat").style.color = "gray";
+        document.getElementById("conver").style.color = "coral"
+        document.getElementById("perfil").style.color = "white";
+        document.getElementById("chat").style.color = "white";
 
         document.getElementById("CONTACTOS").style.display = "block";
         document.getElementById("CHATS").style.display = "none";
         document.getElementById("PERFIL").style.display = "none";
     }else{
         // alert("nooo");
-         document.getElementById("conver").style.color = "brown"
-         document.getElementById("perfil").style.color = "gray";
-         document.getElementById("chat").style.color = "gray";
+         document.getElementById("conver").style.color = "coral"
+         document.getElementById("perfil").style.color = "gainsboro";
+         document.getElementById("chat").style.color = "gainsboro";
 
          document.getElementById("CONVER").style.display = "block"
          document.getElementById("CONTACTOS").style.display = "block";
@@ -60,17 +62,17 @@ function contac(){
 
 function per(){
     if(wid<768){        
-         document.getElementById("perfil").style.color = "brown"
-         document.getElementById("chat").style.color = "gray";
-         document.getElementById("conver").style.color = "gray";
+         document.getElementById("perfil").style.color = "coral"
+         document.getElementById("chat").style.color = "white";
+         document.getElementById("conver").style.color = "white";
 
          document.getElementById("PERFIL").style.display = "block";
          document.getElementById("CHATS").style.display = "none";
          document.getElementById("CONTACTOS").style.display = "none";
          }else{
-              document.getElementById("perfil").style.color = "brown"
-              document.getElementById("chat").style.color = "gray";
-              document.getElementById("conver").style.color = "gray";
+              document.getElementById("perfil").style.color = "coral"
+              document.getElementById("chat").style.color = "white";
+              document.getElementById("conver").style.color = "white";
 
               document.getElementById("CONVER").style.display = "block"
               document.getElementById("PERFIL").style.display = "block";
@@ -94,9 +96,9 @@ function back(){
     if (wid < 768) {
         document.getElementById('abajo').style.display = "block";
 
-        document.getElementById("chat").style.color = "brown"
-        document.getElementById("perfil").style.color = "gray";
-        document.getElementById("conver").style.color = "gray";
+        document.getElementById("chat").style.color = "coral"
+        document.getElementById("perfil").style.color = "white";
+        document.getElementById("conver").style.color = "white";
 
 
         document.getElementById("CHATS").style.display = "block";
@@ -107,14 +109,29 @@ function back(){
         alert("nooo");
     }
 }
+function close(){
+    document.getElementById("nuevoCon").style.display="none";
+}
+function nuevo(){
+    document.getElementById("nuevoCon").style.display = "block";
+}
+function more(){
+    document.getElementById("cuadroChats").style.display="block";
+}
+function more2(){
+    document.getElementById("cuadroChats").style.display = "none";
+}
 
-            var cont = document.getElementById("cont");
+            var tex = document.getElementById("texto");
             var chat = document.getElementById('iconChat');
             var contactos = document.getElementById('iconContactos');
             var perfil = document.getElementById('iconPerfil');
             var atras = document.getElementById('atras');
+            var cerrar = document.getElementById("cerrar");
+            var agregar = document.getElementById("agregarCon");
+            var mas = document.getElementById("mas")
 
-            cont.addEventListener("click", function () {
+            tex.addEventListener("click", function () {
                contenedor();
             });
 
@@ -133,7 +150,18 @@ function back(){
             atras.addEventListener("click",function(){
                 back();
             })
-
+            cerrar.addEventListener("click",function(){
+                close();
+            })
+            agregar.addEventListener("click",function(){
+                nuevo();
+            })
+            mas.addEventListener("click",function(){
+                more();
+            })
+            mas.addEventListener("dblclick",function(){
+                more2();
+            })
 
             ///////////////////////MENSAJES///////////////////////////
 
