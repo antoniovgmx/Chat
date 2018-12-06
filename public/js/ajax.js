a2177ac282e3060b306014e3d317c7b453ff439c
Aixa <aixa.lievana@gmail.com>
2018-12-06 12:00:17 -0600

    ajax
    js

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -1,142 +1,154 @@
-
-
 //////////////CONTACTOS/////////////////////////////////////////////////
-var correo ="aixa@gmail.com";
+var correo = "aixa@gmail.com";
 var idUs = 2
+
+
+function crearElementosContactos(idCon, nomCon) {
+    var contenedores = document.getElementById("contenedoresCon");
+    var contenedor = document.createElement("DIV");
+    contenedor.classList.add("contenedor");
+    contenedor.setAttribute("id", "cont" + idCon + "");
+    var imgCon = document.createElement("DIV")
+    imgCon.classList.add("imgUsuario");
+    var lado = document.createElement("DIV");
+    lado.classList.add("lado");
+    var puntitos = document.createElement("DIV")
+    puntitos.classList.add("mas")
+    puntitos.classList.add("masCon");
+    puntitos.setAttribute("id", "mas2");
+    puntitos.setAttribute('data-menu', 'menuConv');
+    var iconPun = document.createElement("I");
+    iconPun.classList.add("fas")
+    iconPun.classList.add("fa-ellipsis-v");
+    var cuadrito = document.createElement("DIV")
+    cuadrito.classList.add("cuadrito")
+    cuadrito.classList.add("cuadritoChats")
+    var pEliminar = document.createElement("P")
+    pEliminar.classList.add("eliminarChat");
+    pEliminar.setAttribute("id", "eliminarChat");
+    var textEli = document.createTextNode("Eliminar")
+    pEliminar.appendChild(textEli);
+    var pArchivar = document.createElement("P")
+    pArchivar.classList.add("archivarChat");
+    pArchivar.setAttribute("id", "archivarChat");
+    var textArc = document.createTextNode("Archivar")
+    pArchivar.appendChild(textArc);
+    var pFavorito = document.createElement("P")
+    pFavorito.classList.add("favoritoChat");
+    pFavorito.setAttribute("id", "favoritoChat");
+    var textFav = document.createTextNode("Favorito")
+    pFavorito.appendChild(textFav);
+
+    cuadrito.appendChild(pEliminar);
+    cuadrito.appendChild(pArchivar);
+    cuadrito.appendChild(pFavorito);
+    lado.appendChild(puntitos);
+    puntitos.appendChild(iconPun);
+    puntitos.appendChild(cuadrito);
+    lado.appendChild(puntitos);
+    var divtText = document.createElement("DIV");
+    divtText.classList.add("texto")
+    divtText.classList.add("textCon")
+    divtText.setAttribute("onclick", "conversacionNueva(" + idCon + ")");
+    var nombreCon = document.createElement("H3");
+    var nombre = document.createTextNode(nomCon);
+    nombreCon.appendChild(nombre);
+    divtText.appendChild(nombreCon);
+
+    contenedor.appendChild(imgCon);
+    contenedor.appendChild(divtText);
+    contenedor.appendChild(lado);
+    contenedores.appendChild(contenedor);
+}
+
+//////////////////////////AJAX OBTENER CONTACTOS////////////////////////////////
 $.ajax({
     method: "GET",
     url: "http://localhost:3000/inicio/chat/conversaciones/" + idUs
-}).done(function(res){
-    var datos= res.data;
+}).done(function (res) {
+    var datos = res.data;
     datos.map(item => {
         console.log(item);
-        //////////////////////CREACION DE ELEMENTOS///////////////////////////////////////////////
-         var contenedores = document.getElementById("contenedoresCon");
-         var contenedor = document.createElement("DIV");
-         contenedor.classList.add("contenedor");
-         contenedor.setAttribute("id", "cont" + item.idContacto + "");
-         var imgCon = document.createElement("DIV")
-         imgCon.classList.add("imgUsuario");
-         var lado = document.createElement("DIV");
-         lado.classList.add("lado");
-         var puntitos = document.createElement("DIV")
-                puntitos.classList.add("mas")
-                puntitos.classList.add("masCon");
-                puntitos.setAttribute("id","mas2");
-                puntitos.setAttribute('data-menu','menuConv');
-                    var iconPun = document.createElement("I");
-                    iconPun.classList.add("fas")
-                    iconPun.classList.add("fa-ellipsis-v");
-                    var cuadrito = document.createElement("DIV")
-                    cuadrito.classList.add("cuadrito")
-                    cuadrito.classList.add("cuadritoChats")
-                        var pEliminar = document.createElement("P")
-                        pEliminar.classList.add("eliminarChat");
-                        pEliminar.setAttribute("id","eliminarChat");
-                        var textEli = document.createTextNode("Eliminar")
-                            pEliminar.appendChild(textEli);
-                        var pArchivar = document.createElement("P")
-                        pArchivar.classList.add("archivarChat");
-                        pArchivar.setAttribute("id","archivarChat");
-                        var textArc = document.createTextNode("Archivar")
-                            pArchivar.appendChild(textArc);
-                        var pFavorito = document.createElement("P")
-                        pFavorito.classList.add("favoritoChat");
-                        pFavorito.setAttribute("id","favoritoChat");
-                        var textFav = document.createTextNode("Favorito")
-                            pFavorito.appendChild(textFav);
 
-                                cuadrito.appendChild(pEliminar);
-                                cuadrito.appendChild(pArchivar);
-                                cuadrito.appendChild(pFavorito);
-                                lado.appendChild(puntitos);
-                                puntitos.appendChild(iconPun);
-                                puntitos.appendChild(cuadrito);
-                                lado.appendChild(puntitos);
-         var divtText = document.createElement("DIV");
-         divtText.classList.add("texto")
-         divtText.classList.add("textCon")
-          divtText.setAttribute("onclick", "conversacionNueva(" + item.idContacto + ")");
-        var nombreCon = document.createElement("H3");
-        var nombre= document.createTextNode(item.contNombre);
-        nombreCon.appendChild(nombre);
-        divtText.appendChild(nombreCon);
+        var itemNombre = item.contNombre
+        var itemId = item.idContacto
 
-        contenedor.appendChild(imgCon);
-        contenedor.appendChild(divtText);
-        contenedor.appendChild(lado);
-        contenedores.appendChild(contenedor);
-        
+        crearElementosContactos(itemId, itemNombre);
     });
 });
 
+function crearElementosChats(idUsua, nombreUsua) {
+    var contenedores = document.getElementById("contenedores");
+    var contenedor = document.createElement("DIV");
+    contenedor.classList.add("contenedor");
+    contenedor.setAttribute("id", "cont");
+    contenedor.setAttribute("onclick", "chat(" + idUsua + ")");
+    var divImgUs = document.createElement("DIV");
+    divImgUs.classList.add("imgUsuario");
+    var divTexto = document.createElement("DIV");
+    divTexto.classList.add("texto");
+    divTexto.setAttribute("div", "texto");
+    var nombreChat = document.createElement("H3");
+    var nombre2 = document.createTextNode(nombreUsua);
+    nombreChat.appendChild(nombre2);
+    var conversacion = document.createElement("P");
+    var textoConve = document.createTextNode(ultimoMensaje);
+    conversacion.appendChild(textoConve);
+
+    divTexto.appendChild(nombreChat);
+    divTexto.appendChild(conversacion);
+    contenedor.appendChild(divImgUs);
+    contenedor.appendChild(divTexto);
+
+    var divLado = document.createElement("DIV");
+    divLado.classList.add("lado");
+    var divMas = document.createElement("DIV");
+    divMas.classList.add("mas");
+    divMas.setAttribute("div", "mas");
+    var iconMas = document.createElement("I")
+    iconMas.classList.add("fas")
+    iconMas.classList.add("fa-ellipsis-v")
+    var divCuadrito = document.createElement("DIV");
+    divCuadrito.classList.add("cuadrito")
+    divCuadrito.classList.add("cuadroChat")
+    divCuadrito.setAttribute("id", "cuadroChats");
+    var pEliminar = document.createElement("P")
+    pEliminar.classList.add("eliminarChat");
+    pEliminar.setAttribute("id", "eliminarChat");
+    var textEli = document.createTextNode("Eliminar")
+    pEliminar.appendChild(textEli);
+    var pArchivar = document.createElement("P")
+    pArchivar.classList.add("archivarChat");
+    pArchivar.setAttribute("id", "archivarChat");
+    var textArc = document.createTextNode("Archivar")
+    pArchivar.appendChild(textArc);
+    var pFavorito = document.createElement("P")
+    pFavorito.classList.add("favoritoChat");
+    pFavorito.setAttribute("id", "favoritoChat");
+    var textFav = document.createTextNode("Favorito")
+    pFavorito.appendChild(textFav);
+    divCuadrito.appendChild(pEliminar);
+    divCuadrito.appendChild(pArchivar);
+    divCuadrito.appendChild(pFavorito);
+    divMas.appendChild(iconMas);
+    divMas.appendChild(divCuadrito);
+    divLado.appendChild(divMas);
+    contenedor.appendChild(divLado);
+
+    contenedores.appendChild(contenedor);
+}
+///////////////////AJAX OBTENER CONVERSACIONES////////////////
 $.ajax({
     method: "GET",
-    url: "http://localhost:3000/inicio/chat/conversaciones/"+idUs
-}).done(function(res){
+    url: "http://localhost:3000/inicio/chat/conversaciones/" + idUs
+}).done(function (res) {
     var datos = res.data;
-    var ultimoMensaje="holaaa";
+    var ultimoMensaje = "holaaa";
     datos.map(item => {
         console.log(item);
-         var contenedores = document.getElementById("contenedores");
-         var contenedor = document.createElement("DIV");
-         contenedor.classList.add("contenedor");
-         contenedor.setAttribute("id","cont");
-         contenedor.setAttribute("onclick","chat("+item.idUsuario+")");
-                var divImgUs = document.createElement("DIV");
-                divImgUs.classList.add("imgUsuario");
-                var divTexto = document.createElement("DIV");
-                divTexto.classList.add("texto");
-                divTexto.setAttribute("div","texto");
-                    var nombreChat = document.createElement("H3");
-                    var nombre2 = document.createTextNode(item.userNombre);
-                    nombreChat.appendChild(nombre2);
-                    var conversacion = document.createElement("P");
-                    var textoConve = document.createTextNode(ultimoMensaje);
-                    conversacion.appendChild(textoConve);
-
-                    divTexto.appendChild(nombreChat);
-                    divTexto.appendChild(conversacion);
-        contenedor.appendChild(divImgUs);
-        contenedor.appendChild(divTexto);
-
-        var divLado = document.createElement("DIV");
-        divLado.classList.add("lado");
-        var divMas = document.createElement("DIV");
-        divMas.classList.add("mas");
-        divMas.setAttribute("div","mas");
-            var iconMas = document.createElement("I")
-            iconMas.classList.add("fas")
-            iconMas.classList.add("fa-ellipsis-v")
-            var divCuadrito = document.createElement("DIV");
-            divCuadrito.classList.add("cuadrito")
-            divCuadrito.classList.add("cuadroChat")
-            divCuadrito.setAttribute("id","cuadroChats");
-                     var pEliminar = document.createElement("P")
-                     pEliminar.classList.add("eliminarChat");
-                     pEliminar.setAttribute("id", "eliminarChat");
-                     var textEli = document.createTextNode("Eliminar")
-                     pEliminar.appendChild(textEli);
-                     var pArchivar = document.createElement("P")
-                     pArchivar.classList.add("archivarChat");
-                     pArchivar.setAttribute("id", "archivarChat");
-                     var textArc = document.createTextNode("Archivar")
-                     pArchivar.appendChild(textArc);
-                     var pFavorito = document.createElement("P")
-                     pFavorito.classList.add("favoritoChat");
-                     pFavorito.setAttribute("id", "favoritoChat");
-                     var textFav = document.createTextNode("Favorito")
-                     pFavorito.appendChild(textFav);
-                divCuadrito.appendChild(pEliminar);
-                divCuadrito.appendChild(pArchivar);
-                divCuadrito.appendChild(pFavorito);
-                divMas.appendChild(iconMas);
-                divMas.appendChild(divCuadrito);
-        divLado.appendChild(divMas);
-        contenedor.appendChild(divLado);
-
-        contenedores.appendChild(contenedor);
-        
+        var itemIdU = item.idUsuario
+        var itemNomU = item.userNombre
+        crearElementosChats(itemIdU, itemNomU);
     });
 });
 
@@ -176,8 +188,18 @@ buttonEnviar.addEventListener("click", function () {
     ///////////////////////////////scroll hasta abajo//////////////////////////////////////////////////
     var divH = document.getElementById("mensajes").scrollHeight;
     document.getElementById("mensajes").scrollTop = divH;
-
-
-    // console.log("scroll"+divH);
-
 });
+
+function chat(idDestino) {
+    $.ajax({
+        method: "GET",
+        url: "/inicio/chat/mensajes/todos/" + idUs + "/" + idDestino
+    }).done(function (res) {
+        var datos = res.data;
+        var ultimoMensaje = "holaaa";
+        datos.map(item => {
+            console.log(item);
+
+        });
+    });
+}
--- public/js/funciones.js
+++ public/js/funciones.js
@@ -164,8 +164,6 @@ function back(){
                 console.log(this);
                var cuadrito = $(this).find(".cuadrito.cuadritoChats").get(0);
                cuadrito.style.display="block";
-               cuadrito.addEventListener("dblclick",function(){
-                   cuadrito.display="none";
-               })
            });
+           $(document).on("click")
 
